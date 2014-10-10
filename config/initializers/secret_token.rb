48d984bf64f28a3305cd7ecef72325b14999db78
coolwind <ht20092009@126.com>
2014-10-10 10:27:05 +0800

    Initial the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineJudge::Application.config.secret_token = '8d33cb22d4c409a9797f049d3843f77ec28e8db38f5258d38d16f7c4f695e5e37c6796768b3406c53b7ddeaad5c7ef272a71a487da6f64f2fa14de1b04f3d605'
